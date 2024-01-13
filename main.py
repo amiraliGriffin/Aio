7bf9e13f3fde3a21ae6ffa539f7c9a89df8d74eb
amirali <amir.boeing797@gmail.com>
2024-01-13 21:14:43 +0330

    token

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ bot = Client(
     name = "AIO",
     api_id = 29585063,
     api_hash = "7cbc271846d81539d10bdec6cf36ba6a",
-    bot_token = "6369104750:AAHB8bcyMTY1nXOKgn6a4Psm8CXj-yjtFog",
+    bot_token = "6916934962:AAFX076YtdvOh0rUEi4cDzn6enxvaSyVKEY",
     plugins = plug
 )
 
